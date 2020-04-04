570d108afa4d924d43e9e8d39c1fe9c6f64e69c7
kz6fittycent <jimmy.tigert@gmail.com>
2020-04-04 12:14:30 -0500

    Update chromeup.sh

--- chromeup.sh
+++ chromeup.sh
@@ -11,41 +11,44 @@ sleep 3
 # VARIABLES
 #######################################
 
-GET=`sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/getsolus/3rd-party/master/network/web/browser/google-chrome-stable/pspec.xml`
+GRAB=`sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/getsolus/3rd-party/master/network/web/browser/google-chrome-s>
+NEW=`curl https://raw.githubusercontent.com/getsolus/3rd-party/master/network/web/browser/google-chrome-stable/pspec.xml 2>1 /dev/n>
 CURRENT="$(google-chrome-stable --version | cut -d ' ' -f3)"
-NEW="$(ls ~/google-chrome-stable-*.eopkg | cut -d "-" -f4)"
 
 #######################################
 
 clear
 echo ""
-echo "Grabbing new version of Chrome..."
+echo "Checking for latest version of Chrome..."
 echo ""
-$GET
+$NEW
 sleep 3
 
 echo "Comparing versions..."
 echo ""
 echo "Current version is ${CURRENT}"
 echo ""
-echo "Downloaded version is ${NEW}"
+echo "Latest version is ${NEW}"
 
 if [ ${NEW} != ${CURRENT} ]
 then
     echo "Version $NEW is available for installation!"
     sleep 3
     echo ""
-    echo "Installing version $NEW..."
+    echo "Downloading and compiling version $NEW..."
     echo ""
+    $GRAB
     sleep 3
     sudo eopkg it google-chrome-*.eopkg;sudo rm google-chrome-*.eopkg
     echo ""
+    rm $HOME/1
 else
     echo ""
     echo "Version $CURRENT is up to date..."
     echo ""
     sleep 3
-    sudo rm google-chrome-*.eopkg
+    sudo rm $HOME/google-chrome-*.eopkg
+    rm $HOME/1
 fi
 echo ""
 echo "All done!"
